306d7593d1290d3496000e77ffde23228efe3c6b
Aleks-du <71539766+Aleks-du@users.noreply.github.com>
2020-12-11 18:29:01 +0700

    Update lecture08-3.py

--- lecture08/lecture08-3.py
+++ lecture08/lecture08-3.py
@@ -1,11 +1,7 @@
 #Написать функцию, которая принимает в себя два аргумента: строку-шаблон и словарь. Строка-шаблон содержит какой-то шаблон для текста, внутри которого есть маркеры, которые нужно заменить некими словами. В словаре маркеры используются в виде ключей. Вам нужно сделать так, чтобы маркеры в шаблоне заменились на значения, которые хранятся в словаре по этим маркерам.
 
 def Template(template, values):
+    return template.format(**values)
 
-    #template = "Hello, {username}"
-    #"{username}".format(username="values")
-    key, value = list(values.items())[0]
-    #print(value)
-    return template.replace("{username}", value)
-
+template = "Hello, {username}"
 print(Template(template, {"username": "John"}))
